431221692c1756481958a67f1f1eb6c2fbebcba1
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2017-01-09 13:21:46 -0500

    resume zk sessions and update to use new toxiproxy api

--- session/session.go
+++ session/session.go
@@ -59,8 +59,24 @@ type ZKSession struct {
 	log           stdLogger
 }
 
+func ResumeZKSession(servers string, recvTimeout time.Duration, logger stdLogger, clientid *zookeeper.ClientId) (*ZKSession, error) {
+	return newZKSession(servers, recvTimeout, logger, clientid)
+}
+
 func NewZKSession(servers string, recvTimeout time.Duration, logger stdLogger) (*ZKSession, error) {
-	conn, events, err := zookeeper.Dial(servers, recvTimeout)
+	return newZKSession(servers, recvTimeout, logger, nil)
+}
+
+func newZKSession(servers string, recvTimeout time.Duration, logger stdLogger, clientid *zookeeper.ClientId) (*ZKSession, error) {
+	var conn *zookeeper.Conn
+	var events <-chan zookeeper.Event
+	var err error
+
+	if clientid == nil {
+		conn, events, err = zookeeper.Dial(servers, recvTimeout)
+	} else {
+		conn, events, err = zookeeper.Redial(servers, recvTimeout, clientid)
+	}
 	if err != nil {
 		return nil, err
 	}
--- session/session_test.go
+++ session/session_test.go
@@ -3,21 +3,19 @@ package session
 import (
 	"testing"
 	"time"
-
 	toxiproxy "github.com/Shopify/toxiproxy/client"
 )
 
 func TestReceiveEventWhenSubscribing(t *testing.T) {
 	client := toxiproxy.NewClient("http://localhost:8474")
-	proxy := client.NewProxy(&toxiproxy.Proxy{Name: "gozk_test_zookeeper", Listen: "localhost:27445", Upstream: "localhost:2181", Enabled: true})
-
-	err := proxy.Create()
+	proxy, err := client.CreateProxy("gozk_test_zookeeper", "localhost:27445", "localhost:2181")
 	if err != nil {
 		t.Fatal("Couldn't create proxy. Is toxiproxy running? Error: ", err)
 	}
-	defer proxy.Delete()
+	defer proxy.Disable()
 
 	store, err := NewZKSession("localhost:27445", 200*time.Millisecond, nil)
+
 	if err != nil {
 		t.Error("Failed to connect to Zookeeper: ", err)
 	}
@@ -27,7 +25,7 @@ func TestReceiveEventWhenSubscribing(t *testing.T) {
 	store.Subscribe(events)
 
 	go func() {
-		err := proxy.Delete()
+		err := proxy.Disable()
 		if err != nil {
 			t.Error("Failed to delete proxy: ", err)
 		}
@@ -37,7 +35,7 @@ func TestReceiveEventWhenSubscribing(t *testing.T) {
 			t.Error("Expected error when listing children")
 		}
 
-		err = proxy.Create()
+		err = proxy.Enable()
 		if err != nil {
 			t.Error("Failed to create proxy: ", err)
 		}
@@ -48,11 +46,17 @@ func TestReceiveEventWhenSubscribing(t *testing.T) {
 		if event != SessionDisconnected {
 			t.Error("Expected to receive disconnected: ", event)
 		}
+	case <-time.After(5 * time.Second):
+		t.Error("Failed to receive event")
+	}
 
-		if <-events != SessionReconnected {
+	select {
+	case event := <-events:
+		if event != SessionReconnected {
 			t.Error("Expected to receive reconnected: ", event)
 		}
-	case <-time.After(100 * time.Millisecond):
+	case <-time.After(5 * time.Second):
 		t.Error("Failed to receive event")
 	}
 }
+
